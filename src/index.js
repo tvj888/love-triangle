2ca22a8590c3ecf05eba83cc219cc33bc208ec1d
tvj888 <tvj888@gmail.com>
2018-09-24 04:59:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,16 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    var col = 0;
+  for (var x = 0 , lenght = preferences.length; x < lenght;  x++) {
+    var col1 = preferences[preferences[preferences[x]-1]-1]=== x+1;
+    var col2 =x+1 !==preferences[x];
+
+    if (col1 && col2){
+     
+        col++;
+    }
+    }
+    return col / 3;
+  };
 };
